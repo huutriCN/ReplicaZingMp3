b537df799b39f0ae184db997227ade9fff6d72ec
huutriCitynow <huutri.nguyen@m.citynow.vn>
2022-04-29 09:24:51 +0700

    Remove package`

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,4 +1,4 @@
-const custom = require("../webpack.config.js");
+const path = require("path");
 
 module.exports = {
   stories: [
@@ -11,16 +11,22 @@ module.exports = {
     "@storybook/addon-essentials",
     "@storybook/addon-interactions",
     "@storybook/preset-scss",
+    {
+      name: "storybook-addon-next",
+      options: {
+        nextConfigPath: path.resolve(__dirname, "../next.config.js"),
+      },
+    },
   ],
   framework: "@storybook/react",
   core: {
     builder: "@storybook/builder-webpack5",
   },
   staticDirs: ["../public"],
-  webpackFinal: async (config) => {
-    return {
-      ...config,
-      module: { ...config.module, rules: custom.module.rules },
-    };
-  },
+  // webpackFinal: async (config) => {
+  //   return {
+  //     ...config,
+  //     module: { ...config.module, rules: custom.module.rules },
+  //   };
+  // },
 };

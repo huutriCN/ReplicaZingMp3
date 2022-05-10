0b12228acc20cf4ad794ed21b8ad2886fd016a70
huutriCitynow <huutri.nguyen@m.citynow.vn>
2022-05-10 08:51:19 +0700

    Add Large Size for TitleComponent

--- views/Home/components/TitleComponent/index.jsx
+++ views/Home/components/TitleComponent/index.jsx
@@ -1,18 +1,26 @@
+// libs
+import classNames from "classnames";
+
+// components
 // eslint-disable-next-line import/no-extraneous-dependencies
 import { RightOutlined, PlayCircleOutlined } from "@ant-design/icons";
 
+// others
 import styles from "./style.module.scss";
 
-const TitleComponent = ({ title, showedPlay }) => (
-  <div className={styles["component-wrapper"]}>
-    <div className={styles["title-wrapper"]}>
-      <a href="/home" className={styles["title-content"]}>
-        {title}
-      </a>
-      <RightOutlined />
+const TitleComponent = ({ title, showedPlay, size }) => {
+  const style = size === "large" ? styles["size-large"] : "";
+  return (
+    <div className={styles["component-wrapper"]}>
+      <div className={styles["title-wrapper"]}>
+        <a href="/home" className={classNames(styles["title-content"], style)}>
+          {title}
+        </a>
+        <RightOutlined />
+      </div>
+      {showedPlay && <PlayCircleOutlined className={styles["title-icon"]} />}
     </div>
-    {showedPlay && <PlayCircleOutlined className={styles["title-icon"]} />}
-  </div>
-);
+  );
+};
 
 export default TitleComponent;

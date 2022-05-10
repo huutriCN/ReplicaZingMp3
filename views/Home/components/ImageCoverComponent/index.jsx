c822b98f5d9e97e312806d3059a440c11dab1142
huutriCitynow <huutri.nguyen@m.citynow.vn>
2022-05-10 08:50:04 +0700

    Add LargeImage Feature for ImageCoverComponent

--- views/Home/components/ImageCoverComponent/index.jsx
+++ views/Home/components/ImageCoverComponent/index.jsx
@@ -1,20 +1,43 @@
+// libs
+import classNames from "classnames";
 // components
 import SongDescription from "../SongDescription";
 // others
 import styles from "./styles.module.scss";
 
-const ImageCoverComponent = (props) => (
-  <div className={styles["component-wrapper"]}>
-    <a href="/home">
-      <img src="/cover.jpg" alt="" />
-    </a>
-    <SongDescription
-      className={styles["component-desc"]}
-      color="white"
-      {...props}
-    />
-    <span className={styles["component-mask"]} />
-  </div>
-);
+const ImageCoverComponent = ({
+  rankingNumber,
+  artistsNames,
+  newsTitle,
+  title,
+  size = "small",
+  image = "cover.jpg",
+}) => {
+  const sizeImage = size === "large" ? styles["img-large"] : "";
+  return (
+    <div className={classNames(styles["component-wrapper"], sizeImage)}>
+      <a href="/home">
+        <img src={image} alt="" className={styles["component-image"]} />
+      </a>
+      {artistsNames && (
+        <SongDescription
+          className={styles["component-desc"]}
+          color="white"
+          title={title}
+          artistsNames={artistsNames}
+          rankingNumber={rankingNumber}
+        />
+      )}
+      {newsTitle && (
+        <h3 className={styles["component-desc"]}>
+          <a href={newsTitle} className={styles["component-title"]}>
+            {newsTitle}
+          </a>
+        </h3>
+      )}
+      <span className={styles["component-mask"]} />
+    </div>
+  );
+};
 
 export default ImageCoverComponent;

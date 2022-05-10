5bf1ba1d3a5aca9c154f950cd81bdffb995bcedc
huutriCN <huutri.nguyen@m.citynow.vn>
2022-05-10 09:56:22 +0700

    Change Layout of WeeklyMVRanking

--- views/Home/mains/WeeklyMVRanking/index.jsx
+++ views/Home/mains/WeeklyMVRanking/index.jsx
@@ -1,9 +1,41 @@
-import Session from "../../components/Session";
+// Components
+import TabComponent from "../../components/TabComponent";
+import TitleComponent from "../../components/TitleComponent";
+import ImageCoverComponent from "../../components/ImageCoverComponent";
+import MVDescription from "../../components/MVDescription";
+
+// mocks
+import data from "../../../../mocks/WeeklyMVRanking";
+
+// others
 import styles from "./styles.module.scss";
+import ranking from "../../../../dataSources/WeeklyMVRanking";
 
 const WeeklyMVRanking = () => (
   <div className={styles["ranking-wrapper"]}>
-    <Session sessionTitle="Weely MV Ranking" />
+    <TitleComponent title={ranking.title} showedPlay={ranking.showedPlay} />
+    <TabComponent
+      tabList={ranking.tabList}
+      currentActiveTab={ranking.currentActiveTab}
+    />
+    {data[0].songList.map((song, index) => {
+      if (index === 0)
+        return (
+          <ImageCoverComponent
+            rankingNumber={index + 1}
+            showedTools={false}
+            key={song.encodeId}
+            {...song}
+          />
+        );
+      return (
+        <MVDescription
+          key={song.encodeId}
+          rankingNumber={index + 1}
+          {...song}
+        />
+      );
+    })}
   </div>
 );
 

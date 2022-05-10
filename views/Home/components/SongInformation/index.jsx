75ee5f1c4346860da465913056d6601ce32013da
huutriCN <huutri.nguyen@m.citynow.vn>
2022-05-10 09:52:57 +0700

    Wrap Text for SongInfomation

--- views/Home/components/SongInformation/index.jsx
+++ views/Home/components/SongInformation/index.jsx
@@ -1,10 +1,20 @@
+// libs
 import classNames from "classnames";
+
+// others
 import styles from "./styles.module.scss";
 
-const SongInfomation = ({ artistsNames, title, color = "dark" }) => {
+const SongInfomation = ({
+  artistsNames,
+  title,
+  color = "dark",
+  wrapText = true,
+}) => {
   const colorType = color === "dark" ? styles["dark-text"] : "";
+  const wrap = wrapText === true ? styles["wrap-text"] : "";
+
   return (
-    <div className={classNames(styles["infor-wrapper"], colorType)}>
+    <div className={classNames(styles["infor-wrapper"], colorType, wrap)}>
       <h3 className={styles["infor-name"]}>
         <a href={title}>{title}</a>
       </h3>

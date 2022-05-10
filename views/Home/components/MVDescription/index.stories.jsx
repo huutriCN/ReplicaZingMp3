cd4a77ec103584a5add6e424285900336698a3f5
huutriCN <huutri.nguyen@m.citynow.vn>
2022-05-10 09:55:51 +0700

    Feature MV Description

---
+++ views/Home/components/MVDescription/index.stories.jsx
@@ -0,0 +1,12 @@
+import MVDescriptionComponent from ".";
+
+export default {
+  title: "views/Home/components/MVDescriptionCoverComponent",
+  component: MVDescriptionComponent,
+};
+
+export const PrimaryMVDiscription = () => (
+  <MVDescriptionComponent primary>
+    MVDescriptionComponent
+  </MVDescriptionComponent>
+);

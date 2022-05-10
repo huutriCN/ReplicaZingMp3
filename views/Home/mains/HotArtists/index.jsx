c4873bc836334dec5f64c39b7243b831799cb3f5
huutriCN <huutri.nguyen@m.citynow.vn>
2022-05-10 11:31:37 +0700

    Change Layout of HotArtist

--- views/Home/mains/HotArtists/index.jsx
+++ views/Home/mains/HotArtists/index.jsx
@@ -1,9 +1,26 @@
-import Tracking from "../../components/Tracking";
+// components
+import TitleComponent from "../../components/TitleComponent";
+import ArtistComponent from "../../components/ArtistComponent";
+
+// mocks
+import data from "../../../../mocks/HotArtist";
+
+// others
 import styles from "./styles.module.scss";
 
 const HotArtists = () => (
   <div className={styles["artists-wrapper"]}>
-    <Tracking trackingTitle="Hot Artists" />
+    <TitleComponent showedPlay={false} title="Nghệ Sĩ Hot" size="large" />
+    <div className={styles["artists-wrapper-inner"]}>
+      <div className={styles["artist-first"]}>
+        <ArtistComponent {...data[0]} />
+      </div>
+      <div className={styles["artist-small"]}>
+        {data.map((artist) => (
+          <ArtistComponent key={artist.name} {...artist} />
+        ))}
+      </div>
+    </div>
   </div>
 );
 

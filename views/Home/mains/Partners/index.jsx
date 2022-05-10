1d28f569f292d84fc0a129fce8ddd2cdcfdb4a1a
huutriCN <huutri.nguyen@m.citynow.vn>
2022-05-10 11:36:06 +0700

    Change layout of Partners

--- views/Home/mains/Partners/index.jsx
+++ views/Home/mains/Partners/index.jsx
@@ -1,8 +1,21 @@
+/// mocks
+import data from "../../../../mocks/Partners";
+
+// others
 import styles from "./styles.module.scss";
 
 const Partners = () => (
   <div className={styles["partner-wrapper"]}>
-    <div className={styles["partner-wrapper-inner"]}>Partners</div>
+    <div className={styles["partner-wrapper-inner"]}>
+      <span className={styles["partner-title"]}>ĐỐI TÁC</span>
+      <ul className={styles["partner-list"]}>
+        {data.map((partner) => (
+          <li key={partner.thumbnail}>
+            <img src={partner.thumbnail} alt="" />
+          </li>
+        ))}
+      </ul>
+    </div>
   </div>
 );
 

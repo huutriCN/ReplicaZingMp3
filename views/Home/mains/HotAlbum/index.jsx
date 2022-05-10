35f52ed3a4f86ac5a4edb774b512ca0fe1caa5dc
huutriCN <huutri.nguyen@m.citynow.vn>
2022-05-10 10:54:25 +0700

    Change Layout of HotAlbum

--- views/Home/mains/HotAlbum/index.jsx
+++ views/Home/mains/HotAlbum/index.jsx
@@ -1,9 +1,27 @@
-import Tracking from "../../components/Tracking";
+/// components
+import TitleComponent from "../../components/TitleComponent";
+import AlbumComponent from "../../components/AlbumComponent";
+
+// mocks
+import data from "../../../../mocks/HotAlbum";
+
+/// others
+import tracking from "../../../../dataSources/HotAlbum";
 import styles from "./styles.module.scss";
 
 const HotALbum = () => (
   <div className={styles["album-wrapper"]}>
-    <Tracking trackingTitle="Hot Album" />
+    <TitleComponent showedPlay={false} title={tracking.title} size="large" />
+    <div className={styles["content-wrapper"]}>
+      {data.map((albumItem) => (
+        <AlbumComponent
+          key={albumItem.title}
+          imageHeight={tracking.HOTALBUM_IMAGE_HEIGHT}
+          imageWidth={tracking.HOTALBUM_IMAGE_WIDTH}
+          {...albumItem}
+        />
+      ))}
+    </div>
   </div>
 );
 

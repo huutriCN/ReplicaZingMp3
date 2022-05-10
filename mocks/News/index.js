69888b8f46edccdfe33a363f8f4f0894eee0226b
huutriCN <huutri.nguyen@m.citynow.vn>
2022-05-10 10:59:26 +0700

    Mocks Data For News

---
+++ mocks/News/index.js
@@ -0,0 +1,29 @@
+const data = [
+  {
+    title: "Mô hình đào tạo thực tập sinh bậc nhất Hàn Quốc",
+    thumbnail:
+      "https://znews-photo.zingcdn.me/w480/Uploaded/ofh_btgazsox/2022_04_21/aespa_savage_interview_281221.jpg",
+  },
+  {
+    title: "Mỗi đêm nhạc của BTS đem lại 984 triệu USD cho kinh tế Hàn Quốc",
+    thumbnail:
+      "https://znews-photo.zingcdn.me/w480/Uploaded/mfsy2/2022_04_20/ava_jungkook.jpg",
+  },
+  {
+    title: "Quá trình tạo nên những thần tượng tài năng của YG",
+    thumbnail:
+      "https://znews-photo.zingcdn.me/w480/Uploaded/ofh_btgazsox/2022_04_18/lisa_vs_rose_which_diva_sizzles_in_the_flamboyant_earrings_vote_the_best_3.jpg",
+  },
+  {
+    title: "Giải mã Olivia Rodrigo",
+    thumbnail:
+      "https://znews-photo.zingcdn.me/w480/Uploaded/wobvjuz/2022_04_05/olivia_rodrigo_pink_versace_dress_mtv_vmas_2021.jpg",
+  },
+  {
+    title: "T.O.P chia tay Big Bang?",
+    thumbnail:
+      "https://znews-photo.zingcdn.me/w480/Uploaded/wpdhnwhnw/2022_04_05/top_1.jpg",
+  },
+];
+
+export default data;

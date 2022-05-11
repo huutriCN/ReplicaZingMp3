35a2c5e4f9923f2a13e6f4883f904d8560a625fc
huutriCN <huutri.nguyen@m.citynow.vn>
2022-05-11 10:55:40 +0700

    Feature ChartSong Component

---
+++ views/Home/components/ChartSong/index.jsx
@@ -0,0 +1,40 @@
+/// components
+import ImageComponent from "../ImageComponent";
+import NumberComponent from "../NumberComponent";
+
+/// others
+import styles from "./styles.module.scss";
+
+const ChartSong = ({
+  thumbnail,
+  artistsNames,
+  title,
+  rankingNumber,
+  percent,
+}) => (
+  <div className={styles["song-wrapper"]}>
+    <div className={styles["song-wrapper-inner"]}>
+      <NumberComponent rankingNumber={rankingNumber} />
+      <ImageComponent
+        imageHeight="30px"
+        imageWidth="30px"
+        thumbnail={thumbnail}
+      />
+      <div className={styles["song-information"]}>
+        <h3 className={styles["song-title"]}>
+          <a href={title}>{title}</a>
+        </h3>
+        <div className={styles["song-artist"]}>
+          {artistsNames.map((artist, index) => (
+            <h4 key={artist}>
+              <a href={artist}>{index !== 0 ? `, ${artist}` : ` ${artist}`}</a>
+            </h4>
+          ))}
+        </div>
+      </div>
+      <NumberComponent percent={percent} rankingNumber={rankingNumber} />
+    </div>
+  </div>
+);
+
+export default ChartSong;

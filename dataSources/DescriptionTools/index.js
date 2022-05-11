c6def5268bab56f67120d486d505a874c993ab60
huutriCN <huutri.nguyen@m.citynow.vn>
2022-05-11 11:11:43 +0700

    Add DataResource For DescriptionTools

---
+++ dataSources/DescriptionTools/index.js
@@ -0,0 +1,23 @@
+// eslint-disable-next-line import/no-extraneous-dependencies
+import {
+  DownloadOutlined,
+  PlusOutlined,
+  ShareAltOutlined,
+} from "@ant-design/icons";
+
+const tools = [
+  {
+    Tool: DownloadOutlined,
+    title: "Download",
+  },
+  {
+    Tool: PlusOutlined,
+    title: "Add To Favourite",
+  },
+  {
+    Tool: ShareAltOutlined,
+    title: "Share",
+  },
+];
+
+export default tools;

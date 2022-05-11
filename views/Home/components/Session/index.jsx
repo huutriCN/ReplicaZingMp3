e661a575b19287ea1d2cfcd2a0289b084fd372ec
huutriCN <huutri.nguyen@m.citynow.vn>
2022-05-11 13:48:03 +0700

    Deleted Session Component

--- views/Home/components/Session/index.jsx
+++
@@ -1,7 +0,0 @@
-import styles from "./styles.module.scss";
-
-const Session = ({ sessionTitle }) => (
-  <div className={styles["session-wrapper"]}>{sessionTitle}</div>
-);
-
-export default Session;

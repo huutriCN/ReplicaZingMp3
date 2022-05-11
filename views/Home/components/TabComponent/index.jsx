66cac7fc7aaa020d81d73fcb7fd879e83c7a2fbe
huutriCN <huutri.nguyen@m.citynow.vn>
2022-05-11 17:22:26 +0700

    Fixed Conditional ClassNames

--- views/Home/components/TabComponent/index.jsx
+++ views/Home/components/TabComponent/index.jsx
@@ -1,20 +1,21 @@
+// libs
 import classNames from "classnames";
-
+// others
 import styles from "./styles.module.scss";
 
 const TabComponent = ({ tabList, currentActiveTab }) => (
   <ul className={styles["tabs-wrapper"]}>
-    {tabList.map((tab, index) => {
-      const className = currentActiveTab === index ? styles.active : {};
-      return (
-        <li
-          key={tab.link}
-          className={classNames(styles["tab-item"], className)}
-        >
-          <a href={tab.link}>{tab.title}</a>
-        </li>
-      );
-    })}
+    {tabList.map((tab, index) => (
+      <li
+        key={tab.link}
+        className={classNames({
+          [styles["tab-item"]]: true,
+          [styles.active]: currentActiveTab === index,
+        })}
+      >
+        <a href={tab.link}>{tab.title}</a>
+      </li>
+    ))}
   </ul>
 );
 
--- views/Home/components/TitleComponent/index.jsx
+++ views/Home/components/TitleComponent/index.jsx
@@ -8,19 +8,22 @@ import { RightOutlined, PlayCircleOutlined } from "@ant-design/icons";
 // others
 import styles from "./style.module.scss";
 
-const TitleComponent = ({ title, showedPlay, size }) => {
-  const style = size === "large" ? styles["size-large"] : "";
-  return (
-    <div className={styles["component-wrapper"]}>
-      <div className={styles["title-wrapper"]}>
-        <a href="/home" className={classNames(styles["title-content"], style)}>
-          {title}
-        </a>
-        <RightOutlined />
-      </div>
-      {showedPlay && <PlayCircleOutlined className={styles["title-icon"]} />}
+const TitleComponent = ({ title, showedPlay, size }) => (
+  <div className={styles["component-wrapper"]}>
+    <div className={styles["title-wrapper"]}>
+      <a
+        href="/home"
+        className={classNames({
+          [styles["title-content"]]: true,
+          [styles["size-large"]]: size === "large",
+        })}
+      >
+        {title}
+      </a>
+      <RightOutlined />
     </div>
-  );
-};
+    {showedPlay && <PlayCircleOutlined className={styles["title-icon"]} />}
+  </div>
+);
 
 export default TitleComponent;

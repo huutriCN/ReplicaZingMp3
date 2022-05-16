3e425260bcc4d0b05c30d1ceabc0f5fbf027d6cb
huutriCN <huutri.nguyen@m.citynow.vn>
2022-05-16 19:55:34 +0700

    Locales For Multi-Language

---
+++ locales/vi.js
@@ -0,0 +1,12 @@
+const vi = {
+  "Navbar.menu.PersonalMusic": "Nhạc Cá Nhân",
+  "Navbar.menu.chart": "#zingChart",
+  "Navbar.menu.Top100": "Top 100",
+  "Navbar.menu.Topic": "Chủ Đề",
+  "Navbar.menu.Video": "Video",
+  "Navbar.menu.Album": "Album",
+  "Navbar.menu.Artist": "Nghệ sĩ",
+  "Navbar.menu.VIP": "VIP",
+};
+
+export default vi;

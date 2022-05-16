29ed76ebd599171608500a3a34e4c5978f036ffc
huutriCN <huutri.nguyen@m.citynow.vn>
2022-05-16 20:17:10 +0700

    Add Locale Context For App

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,6 +1,9 @@
+// styles
 import "../styles/reset.css";
 import "antd/dist/antd.css";
 import "../styles/global.scss";
+// context
+import { ProvideLocale } from "../contexts/LocaleContext";
 // components
 import Header from "../layouts/Header";
 import Navbar from "../layouts/Navbar";
@@ -8,14 +11,14 @@ import MainLayouts from "../layouts/MainLayouts";
 import Footer from "../layouts/Footer";
 
 const MyApp = ({ Component, pageProps }) => (
-  <>
+  <ProvideLocale>
     <Header />
     <Navbar />
     <MainLayouts>
       <Component {...pageProps} />
     </MainLayouts>
     <Footer />
-  </>
+  </ProvideLocale>
 );
 
 export default MyApp;

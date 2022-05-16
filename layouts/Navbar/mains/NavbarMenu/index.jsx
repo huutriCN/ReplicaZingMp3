287fd37c8d99cff13a9ff1cb7b8a1b7866722036
huutriCN <huutri.nguyen@m.citynow.vn>
2022-05-16 20:21:56 +0700

    Fixed NavBarMenu

--- layouts/Navbar/mains/NavbarMenu/index.jsx
+++ layouts/Navbar/mains/NavbarMenu/index.jsx
@@ -1,13 +1,22 @@
+// context
+import { useLocale } from "../../../../contexts/LocaleContext";
 // others
 import styles from "./styles.module.scss";
 import navbars from "../../../../dataSources/Navbar";
 
-const NavbarMenu = () => (
-  <ul className={styles["menu-wrapper"]}>
-    {navbars.menuItem.map((item) => (
-      <item.itemType {...item.properties} key={item.id} />
-    ))}
-  </ul>
-);
+const NavbarMenu = () => {
+  const { localeDataSource } = useLocale();
+  return (
+    <ul className={styles["menu-wrapper"]}>
+      {navbars.menuItem.map((item) => (
+        <item.itemType
+          itemTitle={localeDataSource[item.properties.title]}
+          {...item.properties}
+          key={item.id}
+        />
+      ))}
+    </ul>
+  );
+};
 
 export default NavbarMenu;

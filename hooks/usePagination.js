84bab6944a990532c17341b1f2bd1953edded75a
huutriCN <huutri.nguyen@m.citynow.vn>
2022-05-17 17:19:54 +0700

    Fixed usePagination hooks

--- hooks/usePagination.js
+++ hooks/usePagination.js
@@ -1,43 +1,45 @@
 // libs
 import { useEffect, useState } from "react";
-
-// KeyCode
-const keyCode = {
-  next: 33,
-  previous: 34,
-};
+// const
+import KEYCODE from "../constants/KeyCode";
 
 const usePagination = ({ total, itemPerPage, isHover }) => {
   const [currentPage, setCurrentPage] = useState(1);
 
-  const handleKey = (e) => {
-    if (e.keyCode === keyCode.next) {
-      e.preventDefault();
-
-      if (currentPage < total / itemPerPage) {
-        setCurrentPage((cur) => cur + 1);
-      }
-    } else if (e.keyCode === keyCode.previous) {
-      e.preventDefault();
-
-      if (currentPage > 1) {
-        setCurrentPage((cur) => cur - 1);
-      }
-    }
-  };
+  const start = (currentPage - 1) * itemPerPage;
+  const end = (currentPage - 1) * itemPerPage + itemPerPage;
 
   const onHandleChange = (page) => {
     setCurrentPage(page);
   };
 
   useEffect(() => {
-    if (isHover) window.addEventListener("keydown", handleKey);
+    const handleKey = (e) => {
+      if (e.keyCode === KEYCODE.PageUp) {
+        e.preventDefault();
+
+        if (currentPage < total / itemPerPage) {
+          setCurrentPage((cur) => cur + 1);
+        }
+      } else if (e.keyCode === KEYCODE.PageDown) {
+        e.preventDefault();
+
+        if (currentPage > 1) {
+          setCurrentPage((cur) => cur - 1);
+        }
+      }
+    };
+
+    if (typeof window === "object") {
+      if (isHover) window.addEventListener("keydown", handleKey);
+    }
+
     return () => {
       window.removeEventListener("keydown", handleKey);
     };
-  }, [currentPage, isHover]);
+  }, [currentPage, isHover, itemPerPage, total]);
 
-  return { currentPage, onHandleChange };
+  return { currentPage, onHandleChange, start, end };
 };
 
 export default usePagination;

1fd4ecc3318758d97c38b9e3cf1ec3d610eece97
huutriCN <huutri.nguyen@m.citynow.vn>
2022-05-22 16:27:57 +0700

    Feature TestProvider

--- utils/TestProvider.jsx
+++ utils/TestProvider.jsx
@@ -1,5 +1,10 @@
+// context
 import { ProvideLocale } from "../contexts/LocaleContext";
+// locales
+import Locales from "@/locales/index";
 
-const Provider = ({ children }) => <ProvideLocale>{children}</ProvideLocale>;
+const Provider = ({ children }) => (
+  <ProvideLocale locales={Locales}>{children}</ProvideLocale>
+);
 
 export default Provider;

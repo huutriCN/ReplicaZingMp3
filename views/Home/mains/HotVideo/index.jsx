3f8088e4a2817c580216fd5bbd611350f9ea863f
huutriCN <huutri.nguyen@m.citynow.vn>
2022-05-26 14:30:53 +0700

    Call actions in HotVideo

--- views/Home/mains/HotVideo/index.jsx
+++ views/Home/mains/HotVideo/index.jsx
@@ -1,7 +1,11 @@
+// libs
+import { useDispatch, useSelector } from "react-redux";
+import { useEffect } from "react";
 /// components
 import TitleComponent from "../../components/TitleComponent";
 import VideoComponent from "../../components/VideoComponent";
-
+// actions
+import { fetchHotVideo } from "@/redux/actions/HotVideo.action";
 // mocks
 import data from "@/mocks/HotVideo";
 
@@ -9,20 +13,29 @@ import data from "@/mocks/HotVideo";
 import tracking from "@/dataSources/HotVideo";
 import styles from "./styles.module.scss";
 
-const HotVideo = () => (
-  <div className={styles["tracking-wrapper"]}>
-    <TitleComponent showedPlay={false} title={tracking.title} size="large" />
-    <div className={styles["content-wrapper"]}>
-      {data.map((videoItem) => (
-        <VideoComponent
-          key={videoItem.title}
-          imageHeight={tracking.HOTVIDEO_IMAGE_HEIGHT}
-          imageWidth={tracking.HOTVIDEO_IMAGE_WIDTH}
-          {...videoItem}
-        />
-      ))}
+const HotVideo = () => {
+  const dispatch = useDispatch();
+  const hotVideos = useSelector((state) => state.hotVideoReducer.hotVideo);
+
+  useEffect(() => {
+    dispatch(fetchHotVideo(data));
+  });
+
+  return (
+    <div className={styles["tracking-wrapper"]}>
+      <TitleComponent showedPlay={false} title={tracking.title} size="large" />
+      <div className={styles["content-wrapper"]}>
+        {hotVideos.map((videoItem) => (
+          <VideoComponent
+            key={videoItem.title}
+            imageHeight={tracking.HOTVIDEO_IMAGE_HEIGHT}
+            imageWidth={tracking.HOTVIDEO_IMAGE_WIDTH}
+            {...videoItem}
+          />
+        ))}
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default HotVideo;

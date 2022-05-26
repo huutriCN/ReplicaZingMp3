7bb8dcac795307b9b9c9b9b1f7387afdd29ef014
huutriCN <huutri.nguyen@m.citynow.vn>
2022-05-26 14:39:49 +0700

    Call actions in Partners

--- views/Home/mains/Partners/index.jsx
+++ views/Home/mains/Partners/index.jsx
@@ -1,22 +1,34 @@
+// libs
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+// actions
+import { fetchPartners } from "@/redux/actions/Partners.action";
 /// mocks
 import data from "@/mocks/Partners";
-
 // others
 import styles from "./styles.module.scss";
 
-const Partners = () => (
-  <div className={styles["partner-wrapper"]}>
-    <div className={styles["partner-wrapper-inner"]}>
-      <span className={styles["partner-title"]}>ĐỐI TÁC</span>
-      <ul className={styles["partner-list"]}>
-        {data.map((partner) => (
-          <li key={partner.thumbnail}>
-            <img src={partner.thumbnail} alt="" />
-          </li>
-        ))}
-      </ul>
-    </div>
-  </div>
-);
+const Partners = () => {
+  const dispatch = useDispatch();
+  const partners = useSelector((state) => state.partnersReducer.partners);
 
+  useEffect(() => {
+    dispatch(fetchPartners(data));
+  });
+
+  return (
+    <div className={styles["partner-wrapper"]}>
+      <div className={styles["partner-wrapper-inner"]}>
+        <span className={styles["partner-title"]}>ĐỐI TÁC</span>
+        <ul className={styles["partner-list"]}>
+          {partners.map((partner) => (
+            <li key={partner.thumbnail}>
+              <img src={partner.thumbnail} alt="" />
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
+};
 export default Partners;

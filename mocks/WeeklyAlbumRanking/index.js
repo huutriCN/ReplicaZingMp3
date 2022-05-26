c71a2fbe8f16538b45ad91078ca53484a0b9b85a
huutriCN <huutri.nguyen@m.citynow.vn>
2022-05-26 14:15:56 +0700

    Fixed mocks data of WeeklyAlbumRanking WeeklyMVRanking WeeklySongRanking

--- mocks/WeeklyAlbumRanking/index.js
+++ mocks/WeeklyAlbumRanking/index.js
@@ -1,154 +1,38 @@
 const data = [
   {
-    sectionTitle: "Việt Nam",
-    songList: [
-      {
-        encodeId: "ZZA07BED",
-        title: "Xuân Hạ Thu Đông, rồi lại Xuân - Tập 9+10",
-        artistsNames: ["Nhiều nghệ sĩ"],
-        thumbnail:
-          "https://photo-resize-zmp3.zmdcdn.me/w240_r1x1_jpeg/cover/6/8/5/e/685e1dbcc1c558aab9e290f70aae8b2b.jpg",
-      },
-      {
-        encodeId: "ZZAICWI6",
-        title: "Xuân Hạ Thu Đông, rồi lại Xuân - Tập 5+6",
-        artistsNames: ["Nhiều nghệ sĩ"],
-        thumbnail:
-          "https://photo-resize-zmp3.zmdcdn.me/w240_r1x1_jpeg/cover/7/d/f/7/7df7cf2a31cb35a945e61a870be5556a.jpg",
-      },
-      {
-        encodeId: "ZZ9OIWA0",
-        title: "Xuân Hạ Thu Đông, rồi lại Xuân - Tập 3+4",
-        artistsNames: ["Nhiều nghệ sĩ"],
-        thumbnail:
-          "https://photo-resize-zmp3.zmdcdn.me/w240_r1x1_jpeg/cover/2/0/8/7/2087a9ffe7a35c22f1efa212d57b0d72.jpg",
-      },
-      {
-        encodeId: "ZZ9OIWa0",
-        title: "Xuân Hạ Thu Đông, rồi lại Xuân - Tập 3+4",
-        artistsNames: ["Nhiều nghệ sĩ"],
-        thumbnail:
-          "https://photo-resize-zmp3.zmdcdn.me/w240_r1x1_jpeg/cover/2/0/8/7/2087a9ffe7a35c22f1efa212d57b0d72.jpg",
-      },
-      {
-        encodeId: "ZZAZZOO6",
-        title: "dreAMEE",
-        artistsNames: ["AMEE"],
-        thumbnail:
-          "https://photo-resize-zmp3.zmdcdn.me/w240_r1x1_jpeg/cover/d/a/9/a/da9a8995a4a7fea43459f5881cb4af14.jpg",
-      },
-    ],
+    encodeId: "ZZA07BED",
+    title: "Xuân Hạ Thu Đông, rồi lại Xuân - Tập 9+10",
+    artistsNames: ["Nhiều nghệ sĩ"],
+    thumbnail:
+      "https://photo-resize-zmp3.zmdcdn.me/w240_r1x1_jpeg/cover/6/8/5/e/685e1dbcc1c558aab9e290f70aae8b2b.jpg",
   },
   {
-    sectionTitle: "US-UK",
-    songList: [
-      {
-        encodeId: "ZZ9AUFD7",
-        title: "As It Was",
-        artistsNames: ["Harry Styles"],
-      },
-      {
-        encodeId: "ZOAWEFCO",
-        title: "Heat Waves",
-        artistsNames: ["Glass Animals"],
-      },
-      {
-        encodeId: "ZU66AZZU",
-        title: "Big Energy",
-        artistsNames: ["Latto"],
-      },
-      {
-        encodeId: "ZUBZDD8D",
-        title: "Enemy (From the series Arcane League of Legends)",
-        artistsNames: ["Imagine Dragons"],
-      },
-      {
-        encodeId: "ZUWIB0AW",
-        title: "STAY",
-        artistsNames: ["The Kid LAROI", "Justin Bieber"],
-      },
-      {
-        encodeId: "ZUIU8DOU",
-        title: "Woman",
-        artistsNames: ["Doja Cat"],
-      },
-      {
-        encodeId: "ZOAZZBZB",
-        title: "Ghost",
-        artistsNames: ["Justin Bieber"],
-      },
-      {
-        encodeId: "ZU6BI00F",
-        title: "THATS WHAT I WANT",
-        artistsNames: ["Lil Nas X"],
-      },
-      {
-        encodeId: "ZUU7FI78",
-        title: "Cold Heart (PNAU Remix)",
-        artistsNames: ["Elton John", "Dua Lipa"],
-      },
-      {
-        encodeId: "ZU9IDI0E",
-        title: "Easy On Me",
-        artistsNames: ["Adele"],
-      },
-    ],
+    encodeId: "ZZAICWI6",
+    title: "Xuân Hạ Thu Đông, rồi lại Xuân - Tập 5+6",
+    artistsNames: ["Nhiều nghệ sĩ"],
+    thumbnail:
+      "https://photo-resize-zmp3.zmdcdn.me/w240_r1x1_jpeg/cover/7/d/f/7/7df7cf2a31cb35a945e61a870be5556a.jpg",
   },
   {
-    sectionTitle: "K-POP",
-    songList: [
-      {
-        encodeId: "ZZ9D7W76",
-        title: "Still Life",
-        artistsNames: ["BIGBANG"],
-      },
-      {
-        encodeId: "ZZ9E07ZD",
-        title: "LOVE DIVE",
-        artistsNames: ["IVE"],
-      },
-      {
-        encodeId: "ZZ8D6ECF",
-        title: "Feel My Rhythm",
-        artistsNames: ["Red Velvet"],
-      },
-      {
-        encodeId: "ZZZ7WODD",
-        title: "Love, Maybe",
-        artistsNames: ["MeloMance"],
-      },
-      {
-        encodeId: "ZZ8Z6WWE",
-        title: "GANADARA (Feat. IU)",
-        artistsNames: ["Jay Park", "IU"],
-      },
-      {
-        encodeId: "ZZUA8FUA",
-        title: "INVU",
-        artistsNames: ["TAEYEON"],
-      },
-      {
-        encodeId: "ZZAOCZOF",
-        title: "LOVE me",
-        artistsNames: ["BE'O"],
-      },
-      {
-        encodeId: "ZUDUFBOB",
-        title: "ELEVEN",
-        artistsNames: ["IVE"],
-      },
-      {
-        encodeId: "ZUBOICZU",
-        title: "If you lovingly call my name",
-        artistsNames: ["GyeongseoYeji", "Jeon Gunho"],
-      },
-      {
-        encodeId: "ZUBOI00E",
-        title: "Traffic light",
-        artistsNames: ["Lee Mujin"],
-      },
-    ],
+    encodeId: "ZZ9OIWA0",
+    title: "Xuân Hạ Thu Đông, rồi lại Xuân - Tập 3+4",
+    artistsNames: ["Nhiều nghệ sĩ"],
+    thumbnail:
+      "https://photo-resize-zmp3.zmdcdn.me/w240_r1x1_jpeg/cover/2/0/8/7/2087a9ffe7a35c22f1efa212d57b0d72.jpg",
+  },
+  {
+    encodeId: "ZZ9OIWa0",
+    title: "Xuân Hạ Thu Đông, rồi lại Xuân - Tập 3+4",
+    artistsNames: ["Nhiều nghệ sĩ"],
+    thumbnail:
+      "https://photo-resize-zmp3.zmdcdn.me/w240_r1x1_jpeg/cover/2/0/8/7/2087a9ffe7a35c22f1efa212d57b0d72.jpg",
+  },
+  {
+    encodeId: "ZZAZZOO6",
+    title: "dreAMEE",
+    artistsNames: ["AMEE"],
+    thumbnail:
+      "https://photo-resize-zmp3.zmdcdn.me/w240_r1x1_jpeg/cover/d/a/9/a/da9a8995a4a7fea43459f5881cb4af14.jpg",
   },
 ];
-
 export default data;

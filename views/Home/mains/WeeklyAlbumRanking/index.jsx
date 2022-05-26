f73bbe446a7b92a8ff97e168e2580d4ad9c8f357
huutriCN <huutri.nguyen@m.citynow.vn>
2022-05-26 14:45:26 +0700

    Call actions in WeeklyAlbumRanking

--- views/Home/mains/WeeklyAlbumRanking/index.jsx
+++ views/Home/mains/WeeklyAlbumRanking/index.jsx
@@ -1,42 +1,60 @@
-// Components
+// libs
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+// components
 import TabComponent from "../../components/TabComponent";
 import TitleComponent from "../../components/TitleComponent";
 import ImageCoverComponent from "../../components/ImageCoverComponent";
 import AlbumDesciption from "../../components/AlbumDescription";
-
+// actions
+import { fetchWeeklyAlbumRanking } from "@/redux/actions/WeeklyAlbumRanking.action";
 // mocks
 import data from "@/mocks/WeeklyAlbumRanking";
-
 // others
 import styles from "./styles.module.scss";
 import ranking from "@/dataSources/WeeklyAlbumRanking";
 
-const WeeklyAlbumRanking = () => (
-  <div className={styles["ranking-wrapper"]}>
-    <TitleComponent title={ranking.title} showedPlay={ranking.showedPlay} />
-    <TabComponent
-      tabList={ranking.tabList}
-      currentActiveTab={ranking.currentActiveTab}
-    />
-    {data[0].songList.map((song, index) => {
-      if (index === 0)
+const WeeklyAlbumRanking = () => {
+  const dispatch = useDispatch();
+  const rankingAlbum = useSelector(
+    (state) => state.rankingAlbumReducer.rankingAlbum,
+  );
+
+  useEffect(() => {
+    dispatch(fetchWeeklyAlbumRanking(data));
+  }, []);
+
+  useEffect(() => {
+    console.log(rankingAlbum);
+  });
+
+  return (
+    <div className={styles["ranking-wrapper"]}>
+      <TitleComponent title={ranking.title} showedPlay={ranking.showedPlay} />
+      <TabComponent
+        tabList={ranking.tabList}
+        currentActiveTab={ranking.currentActiveTab}
+      />
+      {rankingAlbum.map((song, index) => {
+        if (index === 0)
+          return (
+            <ImageCoverComponent
+              rankingNumber={index + 1}
+              showedTools={false}
+              key={song.encodeId}
+              {...song}
+            />
+          );
         return (
-          <ImageCoverComponent
-            rankingNumber={index + 1}
-            showedTools={false}
+          <AlbumDesciption
             key={song.encodeId}
+            rankingNumber={index + 1}
             {...song}
           />
         );
-      return (
-        <AlbumDesciption
-          key={song.encodeId}
-          rankingNumber={index + 1}
-          {...song}
-        />
-      );
-    })}
-  </div>
-);
+      })}
+    </div>
+  );
+};
 
 export default WeeklyAlbumRanking;

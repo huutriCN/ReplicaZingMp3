1551e3180ef902ff51df4a16ef71738b8ead6737
huutriCN <huutri.nguyen@m.citynow.vn>
2022-05-26 14:20:26 +0700

    Call action in ChartRanking Components

--- views/Home/mains/ChartRanking/index.jsx
+++ views/Home/mains/ChartRanking/index.jsx
@@ -1,20 +1,34 @@
+// libs
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
 /// components
 import ChartComponent from "../../components/ChartComponent";
 import RankingSong from "../../components/RankingSong";
-
+// actions
+import { fetchChartRanking } from "@/redux/actions/ChartRanking.action";
 // mocks
 import data from "@/mocks/ChartRanking";
-
 // others
 import styles from "./styles.module.scss";
 
-const ChartRanking = () => (
-  <div className={styles["ranking-wrapper"]}>
-    <ChartComponent songList={data} />
-    {data.map((song, index) => (
-      <RankingSong key={song.title} rankingNumber={index} {...song} />
-    ))}
-  </div>
-);
+const ChartRanking = () => {
+  const dispatch = useDispatch();
+  const rankingSong = useSelector(
+    (state) => state.chartRankingReducer.rankingSong,
+  );
+
+  useEffect(() => {
+    dispatch(fetchChartRanking(data));
+  }, []);
+
+  return (
+    <div className={styles["ranking-wrapper"]}>
+      <ChartComponent songList={rankingSong} />
+      {rankingSong.map((song, index) => (
+        <RankingSong key={song.title} rankingNumber={index} {...song} />
+      ))}
+    </div>
+  );
+};
 
 export default ChartRanking;
